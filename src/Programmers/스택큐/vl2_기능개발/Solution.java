5b3c213bd25de1c8709efc6ba3e56ab2892741d3
syugi90 <altnr23@naver.com>
2021-12-10 17:41:24 +0900

    add solution 스택/큐 기능개발 다른사람코드참고 추가

--- src/Programmers/스택큐/vl2_기능개발/Solution.java
+++ src/Programmers/스택큐/vl2_기능개발/Solution.java
@@ -49,4 +49,18 @@ class Solution {
 
         return answer;
     }
+
+    //다른사람 코드 참고
+//    public int[] solution(int[] progresses, int[] speeds) {
+//        int[] dayOfend = new int[100];
+//        int day = -1;
+//        for(int i=0; i<progresses.length; i++) {
+//            while(progresses[i] + (day*speeds[i]) < 100) {
+//                day++;
+//            }
+//            dayOfend[day]++;
+//        }
+//        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
+//    }
+
 }
